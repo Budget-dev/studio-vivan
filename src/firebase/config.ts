9dec10119f153ff78c205f9abcee7b4e0b696062
Vivan farms <vivanfarmsnatural@gmail.com>
2026-03-29 14:00:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9948939246-dd870",
+  "appId": "1:739491601974:web:abb40d30a92fa446208297",
+  "apiKey": "AIzaSyCCWQm5UKTIRXSS7Ty3_SeBX8tJuUIAekQ",
+  "authDomain": "studio-9948939246-dd870.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "739491601974"
+};
