b7147cc004ec84139950f95cec91fbf01224394b
Vivan farms <vivanfarmsnatural@gmail.com>
2026-04-14 05:09:50 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-9948939246-dd870",
-  "appId": "1:739491601974:web:abb40d30a92fa446208297",
-  "apiKey": "AIzaSyCCWQm5UKTIRXSS7Ty3_SeBX8tJuUIAekQ",
-  "authDomain": "studio-9948939246-dd870.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "739491601974"
+  apiKey: "AIzaSyAgKIEsoHi8ZX0Wn8-yeZpybNQuZKsKe_8",
+  authDomain: "vivanfarms-53957.firebaseapp.com",
+  projectId: "vivanfarms-53957",
+  storageBucket: "vivanfarms-53957.firebasestorage.app",
+  messagingSenderId: "966146202321",
+  appId: "1:966146202321:web:285665f9639d55ed5f83f0",
+  measurementId: "G-6KF7QJ9Z2T"
 };
