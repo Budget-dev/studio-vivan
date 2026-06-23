aab97c4fd9cf72548dbf3fac2985a9c4215ea59b
Vivan farms <vivanfarmsnatural@gmail.com>
2026-06-23 18:04:09 +0000

    You are a Senior Firebase + Next.js debugging engineer.
    My application

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,6 +4,6 @@ export const firebaseConfig = {
   projectId: "vivanfarms-53957",
   storageBucket: "vivanfarms-53957.firebasestorage.app",
   messagingSenderId: "966146202321",
-  appId: "1:966146202321:web:285665f9639d55ed5f83f0",
+  appId: "1:966146202321:web:1b7255c5a82097bf5f83f0",
   measurementId: "G-6KF7QJ9Z2T"
 };
